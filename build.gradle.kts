7a154c261d30e311a580f9c681708b55d30dde22
Oki Sulton <os.alimie@gmail.com>
2026-02-16 21:21:19 +0700

    chore: Update Android Gradle Plugin version
    Upgrades the `com.android.application` plugin from version `9.0.0` to `9.0.1`.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,6 @@
 plugins {
     id("androidx.navigation.safeargs.kotlin") version "2.9.7" apply false
 
-    id("com.android.application") version "9.0.0" apply false
+    id("com.android.application") version "9.0.1" apply false
     id("org.jetbrains.kotlin.android") version "2.3.0" apply false
 }
